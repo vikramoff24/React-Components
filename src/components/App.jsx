aee46adb664ae47e48abf7787f7234f06c7cd3ec
vikramoff24 <60956383+vikramoff24@users.noreply.github.com>
2020-08-28 18:31:39 +0530

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import Heading from "./Heading.jsx";
+import List from "./List";
+function App() {
+  return (
+    <div>
+      <Heading />
+      <List />
+    </div>
+  );
+}
+export default App;
