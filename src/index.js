3de499dbb2df4472f3c0196d9ca3323fa13e8620
Vikram S <60956383+vikramoff24@users.noreply.github.com>
2020-08-28 19:36:20 +0530

    Import & Export

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App.jsx";
-
+import { add, multiply, subtract, divide } from "./calculator.js";
 ReactDOM.render(
   <div>
     <App />
+    {/* for importing and exporting example */}
+    <ul>
+      <p>
+        This is calculated in diffrent module and imported using ES6 module
+        feature
+      </p>
+      <li>{add(1, 2)}</li>
+      <li>{multiply(2, 3)}</li>
+      <li>{subtract(7, 2)}</li>
+      <li>{divide(5, 2)}</li>
+    </ul>
   </div>,
   document.getElementById("root")
 );
---
+++ src/index1.js
@@ -0,0 +1,15 @@
+// Importing & Exporting in ES6
+
+import React from "react";
+import ReactDOM from "react-dom";
+import { add, multiply, subtract, divide } from "./calculator.js";
+
+ReactDOM.render(
+  <ul>
+    <li>{add(1, 2)}</li>
+    <li>{multiply(2, 3)}</li>
+    <li>{subtract(7, 2)}</li>
+    <li>{divide(5, 2)}</li>
+  </ul>,
+  document.getElementById("root1")
+);
